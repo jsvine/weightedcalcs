2c6ec2c9ddd0c9984af9388d800f85bbe69ea62b
Jeremy Singer-Vine <jsvine@gmail.com>
2017-04-08 16:50:40 -0400

    Make minor edits to setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(HERE, NAME, '__version__.py')) as f:
     exec(f.read(), {}, version_ns)
 
 setup(
-    name="weightedcalcs",
+    name=NAME,
     version=version_ns['__version__'],
     description="Pandas-based utility to calculate weighted means, medians, distributions, standard deviations, and more.",
     url="http://github.com/jsvine/weightedcalcs",
@@ -25,7 +25,9 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
