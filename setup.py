a3bf90cfb97dd29b5c4c6565fe45dae36ddf743c
Jeremy Singer-Vine <jsvine@gmail.com>
2024-11-10 13:49:27 -0500

    setup.py classifiers: Simplify to just py3

--- setup.py
+++ setup.py
@@ -25,12 +25,7 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Information Analysis",
